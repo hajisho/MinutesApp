6ec36d5a837e2354161fa1b833459d66b5879931
Nozomu Shimaoka <simaoka@tutanota.com>
2020-06-25 09:28:57 +0900

    /add_messageリソースの追加 /add_messageリソースにPOSTメソッドを追加 /add_messageリソースにPOSTがあったら、メモリにメッセージを追加 フロントエンド側にメッセージを追加するためのフォームを実装 バックエンドへメッセージ送信を実装

--- main.go
+++ main.go
@@ -6,35 +6,44 @@ package main
 
 import (
 	//ginのインポート
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	//"encoding/json"
 	//"fmt"
-	"github.com/jinzhu/gorm"
+
 	_ "github.com/mattn/go-sqlite3" //DBのパッケージだが、操作はGORMで行うため、importだけして使わない
 )
 
+// Message は、一つのメッセージの情報を持った構造体です。
 type Message struct {
-	  //`json:"id"` を　`json: "id"`　にすると読み込めずにエラーなのだが、出力されないので気づかない
-		//なので指定しているはずのに小文字にならないという勘違いが発生する
-    Id   int    `json:"id"`
-    Message string `json:"message"`
+	//`json:"id"` を　`json: "id"`　にすると読み込めずにエラーなのだが、出力されないので気づかない
+	//なので指定しているはずのに小文字にならないという勘違いが発生する
+	// ID は、このメッセージの識別子として導入される値です。
+	ID int `json:"id"`
+	// Message は、このメッセージの文章です。
+	Message string `json:"message"`
 }
 
+// Messages は、複数メッセージのスライスです。
 type Messages []Message
+
+// TODO データベースへ移行後削除
 var messages Messages
+
 //ID用
 var count int
 
-func addMessage(message string){
-	messages = append(messages,Message{count,message})
-	count++;
+func addMessage(message string) {
+	messages = append(messages, Message{count, message})
+	count++
 }
 
-
 func main() {
-	count=0
-	addMessage("餃子")
-	addMessage("チャーハン")
+	count = 0
+
+	// TODO データベースの実装で削除
+	messages = make([]Message, 0)
 
 	//fmt.Printf("(%%#v) %#v\n", messages)
 
@@ -47,6 +56,8 @@ func main() {
 	router.GET("/", handler)
 	// /message に　GETリクエストが飛んできたらfetchMessage関数を実行
 	router.GET("/message", fetchMessage)
+	// /add_messageへのPOSTリクエストは、handleAddMessage関数でハンドル
+	router.POST("/add_message", handleAddMessage)
 	// サーバーを起動しています
 	router.Run(":10000")
 }
@@ -58,6 +69,34 @@ func handler(ctx *gin.Context) {
 }
 
 //messagesに含まれるものを jsonで返す
-func fetchMessage(ctx *gin.Context){
+func fetchMessage(ctx *gin.Context) {
 	ctx.JSON(200, messages)
+}
+
+// AddMessageRequest は、クライアントからのメッセージ追加要求のフォーマットです。
+type AddMessageRequest struct {
+	Message string `json:"message"`
+}
+
+func handleAddMessage(ctx *gin.Context) {
+	// POST bodyからメッセージを獲得
+	req := new(AddMessageRequest)
+	err := ctx.BindJSON(req)
+	if err != nil {
+		// メッセージがJSONではない、もしくは、content-typeがapplication/jsonになっていない
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Malformed request as JSON format is expected"})
+		return
+	}
+
+	if req.Message == "" {
+		// メッセージがない、無効なリクエスト
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Malformed request due to parameter 'message' being empty"})
+		// 帰ることを忘れない
+		return
+	}
+
+	// TODO データベースへの蓄積
+	addMessage(req.Message)
+
+	ctx.JSON(http.StatusOK, gin.H{"success": true})
 }
--- src/hello_world.tsx
+++ src/hello_world.tsx
@@ -1,28 +1,32 @@
 "use strict"
 import React, { useEffect, useState } from "react"
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types';
 
-function GetMessage() {
+import MessagePostForm from './form';
+
+type Message = {
+  id: number,
+  message: string,
+};
+
+type GetMessageResult = Message[];
+
+function GetMessage(props) {
   //このコードではページが表示された時点でこの関数が走るのでuseStateの初期値を与えないと undefined error
   //https://www.debuggr.io/react-map-of-undefined/
   //非同期の結果を残すためのもの？
   //reactは非同期得意ではなかったらしいが, react hook　が出てきて改善したらしい
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<GetMessageResult>([]);
 
   useEffect(() => {
-    // TODO async関数にするか、Promiseで展開する
-    //直でasync関数を受け取れないので一度噛ませる
-    //My comment!!!
-    const res = async() => {
-      //ルート/message　に対して GETリクエストを送る
-      //帰ってきたものをjsonにしてuseStateに突っ込む
-      const r = await fetch("/message")
-                          .then(res => res.json())
-                          .then(setData)
-    }
-    res();
-
-  }, []);
+    // ルート /message に対して GETリクエストを送る
+    // 帰ってきたものをjsonにしてuseStateに突っ込む
+    fetch("/message")
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.log);
+  }, [props.forceUpdate]);
 
   console.log(data)
 
@@ -38,7 +42,32 @@ function GetMessage() {
   );
 }
 
-ReactDOM.render(<GetMessage />, document.getElementById('message'));
+GetMessage.propTypes = {
+  // このランダム値を変更することで、強制的にサーバーからメッセージを取得させ、最新の情報を入手させる
+  forceUpdate: PropTypes.number,
+};
+
+GetMessage.defaultProps = {
+  forceUpdate: Math.random(),
+};
+
+function MessageSection() {
+  const [randomValue, setRandomValue] = useState<number>(Math.random());
+
+  const onMessageAdded = () => {
+    // フォームによってメッセージが追加されたら、メッセージ一覧を更新する
+    setRandomValue(Math.random());
+  };
+
+  return (
+    <>
+      <GetMessage forceUpdate={randomValue} />
+      <MessagePostForm onSubmitSuccessful={onMessageAdded} />
+    </>
+  )
+}
+
+ReactDOM.render(<MessageSection />, document.getElementById('message'));
 
 //文字通り
 //理解が進んだら消す予定
