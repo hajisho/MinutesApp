52abf67944a4e24d1cff261f42a833b19ab14157
Keiichi-S <keiichidori1222@gmail.com>
2020-08-08 16:40:17 +0900

    design of send button and text field

--- src/minutesPage.tsx
+++ src/minutesPage.tsx
@@ -11,85 +11,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import EditMessagePostForm from './editForm';
-
-// メッセージ追加のAPIへのURL
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const API_URL_ADD_MESSAGE = '/add_message';
-
-export default function MessagePostForm(props) {
-  // テキストボックス内のメッセージ
-  const [message, setMessage] = useState<string>('');
-  // サーバがへメッセージ追加のリクエストを処理中ならtrue、でないならfalseの状態
-  const [working, setWorking] = useState<boolean>(false);
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    // FIXME もしかしたら、非同期なため、これが効く前にボタンをクリックできるかもしれない
-    setWorking(true);
-    try {
-      // ページが更新されないようにする
-      event.preventDefault();
-
-      // Reactのハンドラはasyncにできる
-      const res = await fetch(API_URL_ADD_MESSAGE, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // 相応しくないかも
-        // same-originを使うべき？
-        credentials: 'include',
-        body: JSON.stringify({ message }),
-      });
-      const obj = await res.json();
-      if ('error' in obj) {
-        // サーバーからエラーが返却された
-        throw new Error(
-          `An error occurred on querying ${API_URL_ADD_MESSAGE}, the response included error message: ${obj.error}`
-        );
-      }
-      if (!('success' in obj)) {
-        // サーバーからsuccessメンバが含まれたJSONが帰るはずだが、見当たらなかった
-        throw new Error(
-          `An response from ${API_URL_ADD_MESSAGE} unexpectedly did not have 'success' member`
-        );
-      }
-      if (obj.success !== true) {
-        throw new Error(
-          `An response from ${API_URL_ADD_MESSAGE} returned non true value as 'success' member`
-        );
-      }
-      // 要求は成功
-      // リスナ関数を呼ぶ
-      props.onSubmitSuccessful();
-    } finally {
-      setWorking(false);
-      setMessage('');
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={message}
-        type="textbox"
-        placeholder="ここに追加したいメッセージを入力します"
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <button type="submit" disabled={working}>
-        追加
-      </button>
-    </form>
-  );
-}
-
-MessagePostForm.propTypes = {
-  // 新しいメッセージの追加が正常に完了したら呼ばれる関数
-  onSubmitSuccessful: PropTypes.func,
-};
-
-MessagePostForm.defaultProps = {
-  onSubmitSuccessful: () => {},
-};
+import MessagePostForm from './messageForm';
 
 function GetMessage(props) {
   const { forceUpdate } = props;
@@ -151,8 +73,8 @@ function MessageSection() {
 
   return (
     <>
-      <GetMessage forceUpdate={randomValue} />
       <MessagePostForm onSubmitSuccessful={onMessageAdded} />
+      <GetMessage forceUpdate={randomValue} />
     </>
   );
 }
